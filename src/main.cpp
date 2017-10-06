5d3a7a68af98da680cf8ae4ef545affd09347e6e
campsandrew <andrewcamps@dhcp-10-142-209-168.uawifi.arizona.edu>
2017-10-06 02:46:43 -0700

    Current work on first senerio

--- src/main.cpp
+++ src/main.cpp
@@ -1,20 +1,22 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdlib.h>
 #include <iostream>
 #include <cstdio>
 #include <random>
 
-#define DATA_FRAME_SIZE 1500 /* bytes */
-#define SLOT_DURATION 20 /* micro-seconds */
-#define SIFS_DURATION 20 /* micro-seconds */
-#define SIM_DURATION 10 /* seconds */
-#define CW_MAX 1024 /* slots */
+#define DATA_FRAME_SIZE_SLOTS 100 /* slots */
+#define DATA_FRAME_SIZE 12000 /* bits */
+#define CW_MAX 8
 #define CW_0 4 /* slots */
-#define TRANS_RATE 6 /* Mbps */
-#define ACK_SIZE 30 /* bytes */
-#define RTS_SIZE 30 /* bytes */
-#define CTS_SIZE 30 /* bytes */
-#define DIFS_DURATION 40 /* micro-seconds */
+#define SIFS_SIZE 1 /* slots */
+#define ACK_SIZE 2 /* slots */
+#define RTS_SIZE 2 /* slots */
+#define CTS_SIZE 2 /* slots */
+#define DIFS_SIZE 2 /* slots */
+#define SIM_DURATION_CLOCK 500000 /* slots */
+#define SIM_DURATION 10 /* seconds */
+#define SLOT_DURATION 20 /* microseconds */
 
 #define NUM_RATES 4
 #define SLACK 200
@@ -22,18 +24,19 @@
 typedef struct Node_struct {
     int* packet_arrivals[NUM_RATES];
     int dist_len[NUM_RATES];
-    
+    int packet_success[NUM_RATES];
+    int packet_collision[NUM_RATES];
 } Node;
 
-void ScenarioA_CSMA(Node* node1, Node* node2);
+void ScenarioA_CSMA(Node* nodeA, Node* nodeC);
 void ScenarioA_VCS(Node* node1, Node* node2);
 void ScenarioB_CSMA(Node* node1, Node* node2);
 void ScenarioB_VCS(Node* node1, Node* node2);
-int Get_Backoff(void);
+void Data_toFile(char* filename);
+int Get_Backoff(int fails);
 Node* Get_Packet_Dist(int rates[]);
 
 int main(){
-    
     int Frame_Rates[] = {50, 100, 200, 300};
     
     /* Establishing the nodes and packet data */
@@ -49,25 +52,130 @@ int main(){
 	return 0;
 }
 
-void ScenarioA_CSMA(Node* node1, Node* node2){
-    int Clock;
+void ScenarioA_CSMA(Node* nodeA, Node* nodeB){
+    srand(time(NULL)); // Seed the backoff time generator
     
+    /* Test all rates */
+    for(int i = 0; i < NUM_RATES; i++){
+        int Clock = 0;
+        int curPacketA = 0;
+        int curPacketB = 0;
+        int curBackoffA = 0;
+        int curBackoffB = 0;
+        int curCollisionsA = 0;
+        int curCollisionsB = 0;
+        
+        while(Clock < SIM_DURATION_CLOCK){
+            
+            /* Get start time of first pack */
+            if(nodeA->packet_arrivals[i][curPacketA] < nodeB->packet_arrivals[i][curPacketB]){
+                Clock = nodeA->packet_arrivals[i][curPacketA];
+                curBackoffA = Get_Backoff(curCollisionsA); // Gets the backoff time
+                
+                int compPeriodA = DIFS_SIZE + curBackoffA;
+                
+                if((Clock + compPeriodA) <= nodeB->packet_arrivals[i][curPacketB] - 1){  // No other packet able to collide
+                    Clock += compPeriodA + DATA_FRAME_SIZE_SLOTS + SIFS_SIZE + ACK_SIZE;
+                    nodeA->packet_success[i]++; // successful packet transmit
+                    curCollisionsA = 0;
+                    curPacketA++;
+                }else{
+                    curBackoffB = Get_Backoff(curCollisionsB);
+                    
+                    int compPeriodB = DIFS_SIZE + curBackoffB;
+                    
+                    
+                    
+                    /* Check if there is collision */
+                    
+                    /* If no collison, send which packet wins */
+                    
+                }
+                
+                
+            }else if(nodeA->packet_arrivals[i][curPacketA] > nodeB->packet_arrivals[i][curPacketB]){
+                Clock = nodeB->packet_arrivals[i][curPacketB];
+                curBackoffB = Get_Backoff(curCollisionsB); // Gets the backoff time
+                
+                int compPeriod = DIFS_SIZE + curBackoffB;
+                
+                if((Clock + compPeriod) <= nodeA->packet_arrivals[i][curPacketA] - 1){  // No other packet able to collide
+                    Clock += compPeriod + DATA_FRAME_SIZE_SLOTS + SIFS_SIZE + ACK_SIZE; // Advance clock
+                    nodaB->packet_success[i]++; // successful packet transmit
+                    curCollisionsA = 0;
+                    curPacketB++;
+                }else{
+                    
+                    
+                    
+                }
+                
+                
+            }else{
+                Clock = nodeA->packet_arrivals[i][curPacketA];
+                
+                
+                
+            }
+                /* Compare current packets */
+                
+                
+                /* Get lowest slot packet from the two */
+                
+                /* Perform CSMA with DIFS, etc. */
+            
+            
+        }
+    }
 }
 
 void ScenarioA_VCS(Node* node1, Node* node2){
-    int Clock;
+    srand(time(NULL)); // Seed the backoff time generator
+    
+    /* Test all rates */
+    for(int i = 0; i < NUM_RATES; i++){
+        int Clock = 0;
+        
+        
+        
+        
+        
+        
+    }
+    
 }
 
 void ScenarioB_CSMA(Node* node1, Node* node2){
-    int Clock;
+    srand(time(NULL)); // Seed the backoff time generator
+    
+    /* Test all rates */
+    for(int i = 0; i < NUM_RATES; i++){
+        int Clock = 0;
+        
+        
+    }
+    
 }
 
 void ScenarioB_VCS(Node* node1, Node* node2){
-    int Clock;
+    srand(time(NULL)); // Seed the backoff time generator
+    
+    /* Test all rates */
+    for(int i = 0; i < NUM_RATES; i++){
+        int Clock = 0;
+        
+        
+    }
 }
 
-int Get_Backoff(){
-    return 0;
+int Get_Backoff(int fails){
+    int power;
+    
+    power = pow(2, fails);
+    if(fails > CW_MAX) // Prevents going over max contention window
+        power = pow(2, CW_MAX);
+    
+    return rand() % (power * CW_0);
 }
 
 /*
@@ -113,3 +221,8 @@ Node* Get_Packet_Dist(int rates[]){
     
     return node; // Returns the node with packet dist created
 }
+
+void Data_toFile(char* filename){
+    
+}
+
